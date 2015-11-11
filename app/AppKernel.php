421df39f70b21288c0a3ee62ced730f889e0f5c3
Profesor.asix <profesor.asix@gmail.com>
2015-11-11 16:43:26 +0100

    Added dev bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()        
+            new DWS\Bundle\T0Bundle\DWST0Bundle(),
+            new CEED\DWS\T0Bundle\CEEDDWST0Bundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
